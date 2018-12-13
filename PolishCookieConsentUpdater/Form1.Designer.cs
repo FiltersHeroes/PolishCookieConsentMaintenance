a5e51f64a52406cf9230f4cab9ce3ee88b5b9793
hawkeye116477 <hawkeye116477@gmail.com>
2018-12-13 17:11:39 +0100

    First version

---
+++ PolishCookieConsentUpdater/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace PolishCookieConsentUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelPerc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(120, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aktualizuj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(6, 153);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(405, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // labelPerc
+            // 
+            this.labelPerc.AutoSize = true;
+            this.labelPerc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPerc.Location = new System.Drawing.Point(148, 183);
+            this.labelPerc.Name = "labelPerc";
+            this.labelPerc.Size = new System.Drawing.Size(117, 24);
+            this.labelPerc.TabIndex = 5;
+            this.labelPerc.Text = "Pobrano: 0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 261);
+            this.Controls.Add(this.labelPerc);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Aktualizator Polskiej Ciasteczkowej Zgody";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelPerc;
+    }
+}
+
---
+++ PolishCookieConsentUpdater/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows.Forms;
+using System.IO;
+using Ionic.Zip;
+using Octokit;
+using Newtonsoft.Json;
+using System.Reflection;
+
+namespace PolishCookieConsentUpdater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateExt();
+        }
+
+        WebClient webClient;
+
+        string pathDownloadExt = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Downloads\PolishCookieConsent");
+
+
+        public void DownloadFile(string urlAddress, string location)
+        {
+            using (webClient = new WebClient())
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                Uri URL = urlAddress.StartsWith("https://", StringComparison.OrdinalIgnoreCase) ? new Uri(urlAddress) : new Uri("https://" + urlAddress);
+
+                try
+                {
+                    webClient.DownloadFileAsync(URL, location);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                };
+            }
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Update the progressbar percentage only when the value is not the same.
+            progressBar.Value = e.ProgressPercentage;
+
+            // Show the percentage on our label.
+            labelPerc.Text = "Pobrano: " + e.ProgressPercentage.ToString() + "%";
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            var fileTxt = Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath) + @"\install.txt";
+            string pathInstallExt = "";
+            if (File.Exists(fileTxt))
+            {
+
+                pathInstallExt = File.ReadAllText(fileTxt);
+            }
+            else
+            {
+                pathInstallExt = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Rozszerzenia\PolishCookieConsent");
+            }
+            if (e.Error != null)
+            {
+                string error = e.Error.ToString();
+                MessageBox.Show(error);
+                return;
+            }
+
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Pobieranie zostało zatrzymane.");
+            }
+            else
+            {
+                using (ZipFile zip = ZipFile.Read(pathDownloadExt + @"\PolishCookieConsent_chromium.zip"))
+                {
+                    foreach (ZipEntry f in zip)
+                    {
+                        f.Extract(pathInstallExt, ExtractExistingFileAction.OverwriteSilently);
+                    }
+                }
+                Directory.Delete(pathDownloadExt, true);
+                MessageBox.Show("Aktualizacja ukończona!");
+            }
+        }
+
+        public class Manifest
+        {
+            public string version { get; set; }
+        }
+
+        public async void UpdateExt()
+        {
+            var fileTxt = Path.GetDirectoryName(System.Windows.Forms.Application.ExecutablePath) +@"\install.txt";
+            string pathInstallExt = "";
+            if (File.Exists(fileTxt))
+            {
+
+                pathInstallExt = File.ReadAllText(fileTxt);
+            }
+            else
+            {
+                pathInstallExt = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\Rozszerzenia\PolishCookieConsent");
+            }
+            var client = new GitHubClient(new ProductHeaderValue("my-cool-app"));
+            var latestReleaseTag = (await client.Repository.Release.GetLatest("PolishFiltersTeam", "PolishCookieConsent")).TagName;
+            var newVersion = new Version(latestReleaseTag.Replace("v", ""));
+
+
+            Manifest manifest = JsonConvert.DeserializeObject<Manifest>(File.ReadAllText(pathInstallExt + @"\manifest.json"));
+            var oldVersion = new Version(manifest.version);
+
+            if (newVersion > oldVersion)
+            {
+                Directory.CreateDirectory(pathDownloadExt);
+                DownloadFile("https://github.com/PolishFiltersTeam/PolishCookieConsent/releases/download/" + latestReleaseTag + "///PolishCookieConsent_chromium.zip", pathDownloadExt + @"\PolishCookieConsent_chromium.zip");
+            }
+            else
+            {
+                MessageBox.Show("Aktualnie nie ma nowszej wersji rozszerzenia Polska Ciasteczkowa Zgoda.");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateExt();
+        }
+
+    }
+}
